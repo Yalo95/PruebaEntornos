54f9c43329ddd7da6f5df3fde6c328f92b6df193
Yalo95 <ivanylwork@gmail.com>
2026-04-22 16:15:40 +0200

    Update output message in HolaMundo class

--- EjemploGit.java
+++ EjemploGit.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo modificado");
+        System.out.println("Cambio hecho desde la web");        
         System.out.println("Añadiendo información");
     }
 }
